f6ae6245352a14110f95b03bfbcbbe0e810b2d3c
muzi981 <2428412161@qq.com>
2022-04-04 17:47:53 +0800

    1

---
+++ js/talk.js
@@ -0,0 +1,20 @@
+new TypeIt("#tqyxhj", {
+        loop: true,
+        cursorSpeed: 1000,
+        speed: 100
+    })
+    .type("LY && LJ")
+    .pause(2000)
+    .delete(null, {
+        delay: 500
+    })
+    .type("一起走过的第6个520...")
+    .pause(3000)
+    .go();
+
+new TypeIt('#talkTolj', {
+    lifeLike: true,
+    cursorSpeed: 1000,
+    waitUntilVisible: true,
+    speed: 100
+}).go();
